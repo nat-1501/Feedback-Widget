eeb564eed4d6c9b8f5d4ac482157f3e379b2d8ef
Nat-1501 <natalisoares90@gmail.com>
2022-06-14 19:55:09 -0300

    comunicação de componentes

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -36,16 +36,22 @@ export const feedbackTypes = {
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
 
     return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             
 
            {!feedbackType ? (
                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
                
            ) : (
-               <FeedbackContentStep feedbackType={feedbackType} />
+               <FeedbackContentStep 
+               feedbackType={feedbackType}
+               onFeedbackRestartRequested={handleRestartFeedback}
+               />
            )}
 
 
