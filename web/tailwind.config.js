befef554de28c3d047bea4084d3e083334e75212
Nat-1501 <natalisoares90@gmail.com>
2022-06-15 18:49:14 -0300

    criado o botao feedback

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#906DFF',
           500: '#8257e6',
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
