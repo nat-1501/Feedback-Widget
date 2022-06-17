cd230bc9df79023537cd9567a20636825a1dab9f
Nat-1501 <natalisoares90@gmail.com>
2022-06-17 17:56:23 -0300

    Funcionalidade de enviar Feedback finalizada

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { feedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -15,9 +15,20 @@ export function FeedbackContentStep({
     onFeedbackRestartRequested
  }: FeedbackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null)
-    
+    const [comment, setComment] = useState('');
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     
+    function handleSubmitFeedaback(event: FormEvent) {
+        event.preventDefault()
+
+        console.log({
+            screenshot,
+            comment,
+        })
+
+    }
+
     return (
 
         <>
@@ -39,10 +50,11 @@ export function FeedbackContentStep({
                     <CloseButton />
                 </header>
                 
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedaback} className="my-4 w-full">
                 <textarea
                 className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                 placeholder="Conte com detalhes o que está acontecendo.."
+                onChange={event=> setComment(event.target.value)}
                 />
 
                 <footer className="flex gap-2 mt-2">
