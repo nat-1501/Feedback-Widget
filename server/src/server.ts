57c96c8d0bf633585d24dc61adde93d1219008e9
Nat-1501 <natalisoares90@gmail.com>
2022-06-24 22:06:08 -0300

    Separando os arquivos routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,46 +1,12 @@
-import { prisma } from './prisma';
-import nodemailer from 'nodemailer'
 import express from 'express'
+import { routes } from './routes';
+
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "e61c9e105107ea",
-      pass: "805a253c06d004"
-    }
-  });
-
-app.post('/feedbacks', async (req, res) => {
-    const { type, comment, screenshot } = req.body;
-
-
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    await transport.sendMail({
-        from: 'Equipe Feedget <oi@feedget.com>',
-        to: 'Natali Soares <natalisoares90@gmail.com>',
-        html: [
-            `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
-            `<p>Tipo de feedback: ${type}</p>`,
-            `<p>Comentário: ${comment}</p>`, 
-            `</div>`
-        ].join('')
-    });
-
-    return res.status(201).json( {data: feedback} );
-
-});
 
 app.listen(3333, () => {
     console.log('HTTP server runing')
