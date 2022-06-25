6cca632a56818e7373d47b22d6f57b69078391d4
Nat-1501 <natalisoares90@gmail.com>
2022-06-24 21:13:15 -0300

    utilizando mailtrap para enviar email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer'
 import express from 'express'
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "e61c9e105107ea",
+      pass: "805a253c06d004"
+    }
+  });
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -17,6 +27,17 @@ app.post('/feedbacks', async (req, res) => {
         }
     })
 
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Natali Soares <natalisoares90@gmail.com>',
+        html: [
+            `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
+            `<p>Tipo de feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`, 
+            `</div>`
+        ].join('')
+    });
+
     return res.status(201).json( {data: feedback} );
 
 });
@@ -24,4 +45,4 @@ app.post('/feedbacks', async (req, res) => {
 app.listen(3333, () => {
     console.log('HTTP server runing')
 
-})
+});
